70a0883369ac32fc123398228c43943eee7fd44d
Mrsbl465 <50918308+Mrsbl465@users.noreply.github.com>
2021-08-11 23:51:50 -0500

    Revert "Merge branch 'main' of https://github.com/VILLA7523/theoriginscooby"
    This reverts commit bf8f4431c348eae500ba13cf85cab37b51b2a297, reversing
    changes made to 33f9a64b0202a28ebe1d49d87131f4f8b613c731.

--- src/config/dbconnections.js
+++ src/config/dbconnections.js
@@ -1,5 +1,5 @@
 
 const mysql = require('mysql2');
     // create the pool
-const pool = mysql.createPool({host:'localhost', user: 'root', database: 'attendance', password:'contra'});
+const pool = mysql.createPool({host:'localhost', user: 'root', database: 'attendance', password:'buscandoelcambio-5'});
 module.exports = pool;
