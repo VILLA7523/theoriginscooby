9b7b98df74a2c78e9038603650ed02d2b17ffb4b
Mrsbl465 <50918308+Mrsbl465@users.noreply.github.com>
2021-08-11 23:36:08 -0500

    :+1:

--- src/config/dbconnections.js
+++ src/config/dbconnections.js
@@ -1,5 +1,5 @@
 
 const mysql = require('mysql2');
     // create the pool
-const pool = mysql.createPool({host:'localhost', user: 'root', database: 'attendance', password:'buscandoelcambio-5'});
+const pool = mysql.createPool({host:'localhost', user: 'root', database: 'attendance', password:'contra'});
 module.exports = pool;
